be0d1423a541fb781c77f9848384c3dc98047627
Nick McArthur <mcarthurnick@gmail.com>
2016-02-10 14:26:11 -0700

    Finished moveCar problems

--- practice.js
+++ practice.js
@@ -23,7 +23,17 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+  var user =
+  {
+    username: 'Nick',
+    email: 'mac@gmail.com',
+    getUsername: function() {
+      return this.username;
+    }
+  };
+
+  user.getUsername();
+  console.log(user.username);
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
@@ -32,16 +42,26 @@
 
 
 // Write the function definitions which will make the following function invocations function properly.
-
-  //Function Invocations Here
+function Car(carMake, carModel, carYear) {
+  this.move = 0;
+  this.make = carMake;
+  this.model = carModel;
+  this.year = carYear;
+  this.moveCar = function() {
+    return this.move += 10;
+  }
+}
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
 
-prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
+prius.moveCar();
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
-//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+//Hint, you'll need to write a moveCar function which is added to every object that is
+//being returned from the Car function. You'll also need to use the 'this' keyword
+//properly in order to make sure you're invoking moveCar on the right object
+//(prius vs mustang).
 
 
 
@@ -73,7 +93,8 @@ var getMyUsername = function(){
 
 setTimeout(getMyUsername, 5000);
 
-//Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
+//Above you're given an object, a function, and a setTimeout
+//invocation. After 5 seconds, what will the getUsername function return?
 //Note(no tests)
   //Answer Here
 
